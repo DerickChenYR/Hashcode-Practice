070d92b5dc08eb7b8efe5a331cc99bbcce5d60fb
Derick Chen <jockonarock94@gmail.com>
2020-02-19 10:47:46 +0800

    Completed output helper.

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,2 +1,20 @@
 ##!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+
+
+#Write output for the project
+def write_output(number_of_pizzas: int, pizza_types: list):
+
+	OUTPUT_DIR = "../output/"
+
+	with open(OUTPUT_DIR + "output.txt", "w") as file:
+
+		file.write(str(number_of_pizzas) + "\n")
+		for p in pizza_types:
+			file.write(str(p) + " ")
+
+	return True
+
+
