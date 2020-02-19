b5611a5d22333e97bc9e49ecf06366c3c3b2168e
Anand <anandc823@gmail.com>
2020-02-18 22:04:38 -0500

    added sorting

--- scripts/helper.py
+++ scripts/helper.py
@@ -7,6 +7,7 @@ def parse_input(filename):
         maximum_slices = int(lines[0].split()[0])
         num_types = int(lines[0].split()[1])
         sizes = [int(x) for x in lines[1].split()]
+        sizes = sorted(sizes)
     return {"maximum_slices":maximum_slices,"num_types":num_types,"sizes":sizes}
 
 
