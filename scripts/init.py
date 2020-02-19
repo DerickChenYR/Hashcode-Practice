d8b2205251fec6e50358d3fbc14921ed24c4b1f6
Anand <anandc823@gmail.com>
2020-02-18 22:13:38 -0500

    added init changes

--- scripts/init.py
+++ scripts/init.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from helper import parse_input
+from algo import longest_subarray_less_k
 import sys
 
 def main():
     filename = sys.argv[1]
     data = parse_input(filename)
-    print(data)
+    print(longest_subarray_less_k(data["maximum_slices"],data["sizes"]))
 main()
