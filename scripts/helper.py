bdf65e20c7927934ed24299bac5ed7338fb2a3c4
Anand <anandc823@gmail.com>
2020-02-18 21:49:56 -0500

    parse_input_helper

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,2 +1,10 @@
 ##!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+def parse_input(filename):
+    with open("filename") as f:
+        lines = f.readlines()
+        maximum_slices = lines[0].split()[0]
+        num_types = lines[0].split()[1]
+        sizes = lines[1].split()
+    return maximum_slices,num_types,sizes
--- scripts/init.py
+++ scripts/init.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from "./helper" import parse_input
+def main():
+    filename = sys.argv[0]
+    parse_input(filename)
