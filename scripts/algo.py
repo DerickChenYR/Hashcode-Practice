33f868c8c61b0ad877b16b5ec99db8eb8ebe03b6
Derick Chen <jockonarock94@gmail.com>
2020-02-19 12:58:34 +0800

    New backtracking algo.

--- scripts/algo.py
+++ scripts/algo.py
@@ -35,8 +35,62 @@ def longest_subarray_less_k(k:int, pizzas: list):
 
 
 
-'''
+
+#DFS into the array use backtrack, terminate when current sum exceeds target
+def combo_sum_backtrack(k:int, pizzas: list):
+
+	ret = []
+	max_type = -1	#make this a global var? So to reduce repetitions in the root recursive processes
+
+	max_type, combinations = backtrack(0, pizzas, k, [], max_type, ret)
+	
+	max_points = -1
+	best_choice = []
+	for tup in combinations:
+		if sum(tup) > max_points:
+			max_points = sum(tup)
+			best_choice = tup
+
+
+	#Get the indices for the best choice of pizzas
+	best_indices = []
+	for i in best_choice:
+		best_indices.append(pizzas.index(i))
+
+	return max_type, best_indices
+
+def backtrack(ind, pizzas, k, cur_list, best, ret):
+
+	if sum(cur_list) == k:
+
+		best = len(cur_list)
+		#ret.append(tuple(cur_list))
+		return best, cur_list
+
+	elif sum(cur_list) > k:
+		return -1, []
+
+	#Case sublist sum less than k
+	else:
+		best = len(cur_list)
+		for i in range(ind , len(pizzas), 1):
+
+			cur_list.append(pizzas[i])
+			
+			new_best, new_ret = backtrack(i+1, pizzas, k, cur_list, best, ret)
+
+			if new_best >= best:
+				#print(f"new best is {new_best}, best is {best}")
+				best = new_best
+				ret.append(tuple(cur_list))
+
+			cur_list.pop()
+
+	return best, ret
+
+	
+#'''
 #Test Ground
-ret = longest_subarray_less_k(8, [1,2,3,4,5])
-print(ret)
-'''
+#ret = combo_sum_backtrack(17, [2, 5, 6, 8])
+#print(ret)
+#'''
